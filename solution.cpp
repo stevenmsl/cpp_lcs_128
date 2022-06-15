12e58c4fb032cf6577e148a412ca49060ada7743
Steven Lin <steven_lx@outlook.com>
2022-06-14 21:08:17 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -15,15 +15,16 @@ using namespace std;
 
 /*takeaways
   - To achieve O(n), we need to use a hash table to store the length of
-    a consecutive subsequence at the bounds
+    a consecutive subsequence at both ends
   - [1,2,4,5,3]
     - scan in 1, nothing exists in the hash table yet so map[1] = 1
       this means the subsequnce ends or starts with 1 has a length
       of 1
-    - scan in 2, 1 is on its left, so left = map[1] = 1. nothing on
-      2's right, so right = 0, so the total length of adding 2 in
-      is left + right + 1 = 2 representing the subsequence "12".
-      We have to update map[1]=map[2]=2
+    - scan in 2, we are looking for the length of the subsequence
+      that ends at 1, map[1], and the length of the subsequence
+      starts at 3, which nothing can be found. We then need
+      to update map[2-left]=map[1]= 1 + 1, and map[2+right]=map[2]=2
+
     - scan in 4 and 5. We now have map[4] = map[5] = 2 for the same reason
     - scan in 3.
       - map[3-1] = 2 means the subsequence ends at 2 has a length of 2
@@ -40,12 +41,17 @@ int Solution::longestConsecutive(vector<int> &nums)
 
   for (auto n : nums)
   {
+    /* n-1, n, n+1 */
     auto left = m.find(n - 1), right = m.find(n + 1);
     auto l = left != m.end() ? left->second : 0;
     auto r = right != m.end() ? right->second : 0;
     /* one is contributed by adding the number n */
     auto t = l + r + 1;
-    /* update the bounds */
+    /* update the start and end
+       - this represents the subsequnce
+         (n-l)...n...(n+r)
+       - length: n + r - (n - l) + 1 = r + l + 1
+    */
     m[n - l] = m[n + r] = t;
   }
 
